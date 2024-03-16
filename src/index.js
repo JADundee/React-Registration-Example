d2b74e33e93ae6beb882949674d96eecf1e416ce
JADundee <Jadundee@icloud.com>
2024-03-16 08:01:38 -0600

    Registration Form complete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Routes>
+          <Route path='/*' element={<App />}/>
+        </Routes>
+      </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  
 );
