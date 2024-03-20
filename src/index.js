24f7e89432279fd6320d92c236233e645225a7be
JADundee <Jadundee@icloud.com>
2024-03-20 09:44:33 -0600

    Protected routes and user roles

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import  { AuthProvider } from './context/AuthProvider';
+
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <Router>
+      <AuthProvider>
         <Routes>
-          <Route path='/*' element={<App />}/>
+          <Route path="/*" element={<App />} />
         </Routes>
-      </Router>
-  </React.StrictMode>,
-  
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>
 );
